e7d279fdb1083e183a901c9c03c03d346d232e43
Albert <ayanit1@googlemail.com>
2017-03-31 11:00:12 +0100

    Change index.js file to show single notes

--- spec/view-model-spec.js
+++ spec/view-model-spec.js
@@ -1,28 +1,20 @@
 test({
 
-beforeEach: function() {
-var list = new List();
-list.addNote('This is the first note');
-list.addNote('This is the second note');
-var listView = new View(list);
-},
-
   viewInstantiatesWithList: function() {
     var list = new List();
     list.addNote('This is the first note');
     list.addNote('This is the second note');
     var listView = new View(list);
   assert.isTrue(listView._noteList === list, 'viewInstantiatesWithList');
-},
+  },
 
-viewReturnsHTML: function() {
-  var list = new List();
-  list.addNote('This is the first note');
-  list.addNote('This is the second note');
-  var listView = new View(list);
-  assert.isTrue(
-    listView.returnHTML() === '<ul><li><div>This is the first note</div></li><li><div>This is the second note</div></li></ul>',
-  'viewReturnsHTML');
-}
-
+  viewReturnsHTML: function() {
+    var list = new List();
+    list.addNote('This is the first note');
+    list.addNote('This is the second note');
+    var listView = new View(list);
+    assert.isTrue(
+      listView.returnHTML() === '<ul><li><div>This is the first note</div></li><li><div>This is the second note</div></li></ul>',
+    'viewReturnsHTML');
+  }
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 var noteList = new List()
-
 noteList.addNote("Favourite drink: seltzer")
+
 var view = new View(noteList)
 var controller = new NoteController(noteList)
 
-
-controller.getHTML(view)
+controller.insertHTML(view)
